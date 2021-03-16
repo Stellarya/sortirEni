669b7896cc01467cbe71df850b7ab20288d17d90
Ronan <delasalle.sio.derrien.r@gmail.com>
2021-03-16 23:56:51 +0100

    Gestion des filtres de sorties fonctionnelle (reste à gérer les états)

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -14,6 +14,7 @@ use App\Repository\ParticipantRepository;
 use App\Repository\SiteRepository;
 use App\Repository\SortieRepository;
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,25 +23,133 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
+    public $siteID = null;
     /**
      * @Route("/sortie", name="page_sortie")
      * @param Request $request
      * @param SortieRepository $sortieRepository
+     * @param UserRepository $userRepository
      * @return Response
      */
-    public function liste(Request $request, SortieRepository $sortieRepository): Response
+    public function liste(Request $request, SortieRepository $sortieRepository, UserRepository $userRepository): Response
     {
-        $sorties = $sortieRepository->findAll();
-
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $sorties = $sortieRepository->findSortiesPubliees();
+        $participantConnecte = $userRepository->findOneBy(["username" => $this->getUser()->getUsername()])->getParticipant();
+        $this->siteID = $participantConnecte->getEstRattacheA()->getId();
         $form = $this->createForm(SortieFiltreType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid())
         {
 
+            $data = $form->getData();
+            $site = isset($data["site"]);
+            $txtRecherche = isset($data["nom_recherche"]);
+            $dateDebut = isset($data["dateDebut"]);
+            $dateFin = isset($data["dateFin"]);
+            $estOrganisateur = $data["estOrganisateur"];
+            $estInscrit = $data["estInscrit"];
+            $estPasInscrit = $data["estPasInscrit"];
+            $estSortiePassee = $data["estSortiePassee"];
+            $dateJour = null;
+            $this->siteID = $data["site"]->getId();
+            if($txtRecherche || $dateDebut || $dateFin || $estOrganisateur || $estInscrit || $estPasInscrit || $estSortiePassee)
+            {
+                $sorties = [];
+
+                if($txtRecherche)
+                {
+                    $texte = $data["nom_recherche"];
+                    $sortiesConcernees = $sortieRepository->findSortiesParTexte($texte);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                }
+
+                if($dateDebut && $dateFin)
+                {
+                    $dateD = $data["dateDebut"];
+                    $dateF = $data["dateFin"];
+                    $sortiesConcernees = $sortieRepository->findSortiesEntreDeuxDates($dateD, $dateF);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                } elseif ($dateDebut)
+                {
+                    $dateD = $data["dateDebut"];
+                    $sortiesConcernees = $sortieRepository->findSortiesApresUneDate($dateD);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                } elseif ($dateFin)
+                {
+                    $dateF = $data["dateFin"];
+                    $sortiesConcernees = $sortieRepository->findSortiesAvantUneDate($dateF);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                }
+
+                if ($estOrganisateur || $estInscrit || $estPasInscrit)
+                {
+                    $participantConnecte = $userRepository->findOneBy(["username" => $this->getUser()->getUsername()]
+                    )->getParticipant();
+                    $participantID = $participantConnecte->getID();
+                    if ($estOrganisateur) {
+                        $sortiesConcernees = $sortieRepository->findSortiesByOrganisateur($participantID);
+                        $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                    }
+                    if ($estInscrit) {
+                        $sortiesConcernees = $sortieRepository->findSortiesByParticipant($participantID);
+                        $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                    }
+                    if ($estPasInscrit) {
+                        $sortiesConcernees = $sortieRepository->findSortiesByParticipantPasInscrit($participantID);
+                        $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                    }
+                }
+
+                if($estSortiePassee)
+                {
+                    $dateJour = new DateTime('NOW');
+                    $date = date_format($dateJour, "Y-m-d G:i:s");
+                    $sortiesConcernees = $sortieRepository->findSortiesParDatePassee($date);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                }
+
+            } else
+            {
+                if($this->siteID == $data["site"]->getId()) {
+                    $sorties = [];
+                    $sortiesConcernees = $sortieRepository->findSortiesParSite($this->siteID);
+                    $sorties = $this->ajoutUniqueAuTableau($sortiesConcernees, $sorties);
+                }
+            }
+
         }
+
+        usort($sorties, function($a, $b) {
+            $ad = $a->getDateHeureDebut();
+            $bd = $b->getDateHeureDebut();
+
+            if ($ad == $bd) {
+                return 0;
+            }
+
+            return $ad < $bd ? -1 : 1;
+        });
+
         return $this->render('sortie/liste.html.twig', ["sorties" => $sorties, "form" => $form->createView(),
                                     "title" => "Liste des sorties"
         ]);
+    }
+
+    /**
+     * @param array $sortiesConcernees
+     * @param array $sorties
+     * @return array
+     */
+    public function ajoutUniqueAuTableau(array $sortiesConcernees, array $sorties): array
+    {
+        foreach ($sortiesConcernees as $sortie) {
+            if (!in_array($sortie, $sorties) && $sortie->getSite()->getId() == $this->siteID) {
+                $sorties[] = $sortie;
+            }
+        }
+
+        return $sorties;
     }
 
     /**
@@ -103,4 +212,40 @@ class SortieController extends AbstractController
 
         return $this->render('sortie/detail.html.twig', ["sortie" => $sortie]);
     }
+
+    function array_sort($array, $on, $order=SORT_ASC)
+    {
+        $new_array = array();
+        $sortable_array = array();
+
+        if (count($array) > 0) {
+            foreach ($array as $k => $v) {
+                if (is_array($v)) {
+                    foreach ($v as $k2 => $v2) {
+                        if ($k2 == $on) {
+                            $sortable_array[$k] = $v2;
+                        }
+                    }
+                } else {
+                    $sortable_array[$k] = $v;
+                }
+            }
+
+            switch ($order) {
+                case SORT_ASC:
+                    asort($sortable_array);
+                    break;
+                case SORT_DESC:
+                    arsort($sortable_array);
+                    break;
+            }
+
+            foreach ($sortable_array as $k => $v) {
+                $new_array[$k] = $array[$k];
+            }
+        }
+
+        return $new_array;
+    }
+
 }
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -19,23 +19,146 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    // /**
-    //  * @return Sortie[] Returns an array of Sortie objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+    /**
+     * @return int|mixed|string
+     */
+    public function findSortiesParSite($siteID){
+        $qb = $this->createQueryBuilder('s');
+        $qb->join('s.etat', 'e')
+            ->where($qb->expr()->eq('e.id', '2'))
+            ->join('s.site', 'si')
+            ->andWhere($qb->expr()->eq('si.id', '?1'));
+        $query = $qb->getQuery()->setParameter(1, $siteID);
+        return $query->getResult();
     }
-    */
 
+    /**
+     * @param $id
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesByOrganisateur($id): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->join('s.organisateur', 'o')
+            ->join('s.etat', 'e')
+            ->where($qb->expr()->eq('o.id', $id))
+            ->andWhere($qb->expr()->eq('e.id', '2'));
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param $texte
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesParTexte($texte): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where($qb->expr()->like('s.nom', '?1'));
+        $query = $qb->getQuery();
+        $query->setParameter(1, '%'.$texte.'%');
+        return $query->getResult();
+    }
+
+    /**
+     * @param $dateDebut
+     * @param $dateFin
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesEntreDeuxDates($dateDebut, $dateFin): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where($qb->expr()->gte('s.dateHeureDebut', '?1'))
+            ->andWhere($qb->expr()->lte('s.dateHeureDebut', '?2'));
+        $query = $qb->getQuery();
+        $query->setParameter(1, $dateDebut);
+        $query->setParameter(2, $dateFin);
+        return $query->getResult();
+    }
+
+    /**
+     * @param $dateDebut
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesApresUneDate($dateDebut): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where($qb->expr()->gte('s.dateHeureDebut', '?1'));
+        $query = $qb->getQuery();
+        $query->setParameter(1, $dateDebut);
+        return $query->getResult();
+    }
+
+    /**
+     * @param $dateFin
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesAvantUneDate($dateFin): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where($qb->expr()->lte('s.dateHeureDebut', '?1'));
+        $query = $qb->getQuery();
+        $query->setParameter(1, $dateFin);
+        return $query->getResult();
+    }
+
+    /**
+     * @param $date
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesParDatePassee($date): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->where($qb->expr()->lt('s.dateHeureDebut', '?1'));
+        $query = $qb->getQuery();
+        $query->setParameter(1, $date);
+        return $query->getResult();
+    }
+
+    /**
+     * @param $id
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesByParticipant($id): array
+    {
+        $qb = $this->createQueryBuilder('s');
+        $qb->join('s.participants', 'p')
+            ->join('s.etat', 'e')
+            ->where($qb->expr()->eq('p.id', $id))
+            ->andWhere($qb->expr()->eq('e.id', '2'));
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param $id
+     * @return Sortie[] Returns an array of Sortie objects
+     */
+    public function findSortiesByParticipantPasInscrit($id): array
+    {
+        $dql = "SELECT s
+                FROM App\Entity\Sortie s
+                WHERE s.id NOT IN (
+                    SELECT so
+                    FROM App\Entity\Sortie so
+                    JOIN so.participants pa
+                    WHERE pa.id = ?1
+                )";
+
+        $em = $this->getEntityManager();
+        $query = $em->createQuery($dql);
+        $query->setParameter(1, $id);
+
+        return $query->getResult();
+    }
+
+    /**
+     * @return int|mixed|string
+     */
+    public function findSortiesPubliees(){
+        $qb = $this->createQueryBuilder('s');
+        $qb->join('s.etat', 'e')
+            ->where($qb->expr()->eq('e.id', '2'));
+        return $qb->getQuery()->getResult();
+    }
     /*
     public function findOneBySomeField($value): ?Sortie
     {
