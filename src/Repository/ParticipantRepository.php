02c8115419ed0f15e8f1fb6178436d8157909594
Marie Evanno <marie.evanno@adventiel.fr>
2021-03-17 22:55:21 +0100

    desinscription OK

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Participant;
+use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Driver\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,22 +21,25 @@ class ParticipantRepository extends ServiceEntityRepository
         parent::__construct($registry, Participant::class);
     }
 
-    // /**
-    //  * @return Participant[] Returns an array of Participant objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param Sortie $oSortie
+     * @return Participant[] Returns an array of Sortie objects
+     * @throws Exception|\Doctrine\DBAL\Exception
+     */
+    public function findParticipantsBySortie(Sortie $oSortie): array
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+        $connexion = $this->getEntityManager()->getConnection();
+
+        $sql = "SELECT p.*
+                FROM participant p
+                INNER JOIN participant_sortie ps ON ps.participant_id = p.id
+                INNER JOIN sortie s ON s.id = ps.sortie_id
+                WHERE ps.sortie_id = :idSortie";
+
+        $stmt = $connexion->prepare($sql);
+        $stmt->execute(['idSortie' => $oSortie->getId()]);
+        return $stmt->fetchAll();
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Participant
